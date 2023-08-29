9364c11f8c8c4ab06328c2d784a515601a6d99dc
AbelZaum <127804266+AbelZaum@users.noreply.github.com>
2023-08-29 18:54:56 -0300

    Add files via upload

---
+++ ArraysSort.java
@@ -0,0 +1,21 @@
+package math;
+
+import java.util.Arrays;
+import javax.swing.JOptionPane;
+
+public class ArraysSort {
+    
+    public static void main(String[] args) {
+        
+        int[] numeros = new int[5];
+        for (int i = 0; i < 5; i++) {
+            
+            numeros[i] = Integer.parseInt(JOptionPane.showInputDialog("Informe um número: " + (i + 1)));
+            
+        }
+        JOptionPane.showMessageDialog(null, Arrays.toString(numeros));
+        Arrays.sort(numeros);
+        JOptionPane.showMessageDialog(null, Arrays.toString(numeros));
+        
+    }
+}
