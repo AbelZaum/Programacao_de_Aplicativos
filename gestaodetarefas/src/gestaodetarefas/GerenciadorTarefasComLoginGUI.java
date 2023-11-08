609e27deef2bb8aed741f5b04c810e23ff2fea8c
Gabriel Abel da Silva <127804266+AbelZaum@users.noreply.github.com>
2023-11-08 17:30:45 -0300

    Update GerenciadorTarefasComLoginGUI.java

--- gestaodetarefas/src/gestaodetarefas/GerenciadorTarefasComLoginGUI.java
+++ gestaodetarefas/src/gestaodetarefas/GerenciadorTarefasComLoginGUI.java
@@ -1,14 +1,12 @@
-package gestaodetarefas;
-
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import javax.swing.table.DefaultTableModel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class GerenciadorTarefasComLoginGUI {
 
@@ -20,10 +18,9 @@ public class GerenciadorTarefasComLoginGUI {
     private JTable tarefaTable;
     private List<Tarefa> tarefas;
 
-    private JPanel loginPanel;
-    private JTextField usernameField;
-    private JPasswordField passwordField;
-    private boolean loggedIn = false;
+    private JComboBox<String> estadoComboBox;  // JComboBox para selecionar o estado
+    private JButton adicionarTarefaButton;  // Botão para adicionar tarefas
+    private boolean loggedIn = true;  // Defina como true para evitar a tela de login
 
     public GerenciadorTarefasComLoginGUI() {
         frame = new JFrame("Gerenciador de Tarefas");
@@ -31,7 +28,7 @@ public class GerenciadorTarefasComLoginGUI {
         frame.setSize(800, 600);
 
         tarefas = new ArrayList<>();
-        tarefaTableModel = new DefaultTableModel(new String[]{"Nome", "Descrição", "Data de Vencimento"}, 0);
+        tarefaTableModel = new DefaultTableModel(new String[]{"Nome", "Descrição", "Data de Vencimento", "Estado"}, 0);
         tarefaTable = new JTable(tarefaTableModel);
         tarefaTable.setRowHeight(40);
         tarefaTable.getTableHeader().setReorderingAllowed(false);
@@ -55,7 +52,10 @@ public class GerenciadorTarefasComLoginGUI {
         descricaoTarefaField = new JTextField();
         JLabel dataVencimentoLabel = new JLabel("Data de Vencimento (dd/MM/yyyy):");
         dataVencimentoField = new JTextField();
-        JButton adicionarTarefaButton = new JButton("Adicionar Tarefa");
+
+        estadoComboBox = new JComboBox<>(new String[]{"A fazer", "Em andamento", "Concluídas"});
+
+        adicionarTarefaButton = new JButton("Adicionar Tarefa");
         adicionarTarefaButton.setBackground(Color.GREEN);
         adicionarTarefaButton.setForeground(Color.WHITE);
         adicionarTarefaButton.addActionListener(new ActionListener() {
@@ -64,13 +64,15 @@ public class GerenciadorTarefasComLoginGUI {
                 adicionarTarefa();
             }
         });
+
         entradaPainel.add(nomeTarefaLabel);
         entradaPainel.add(nomeTarefaField);
         entradaPainel.add(descricaoTarefaLabel);
         entradaPainel.add(descricaoTarefaField);
         entradaPainel.add(dataVencimentoLabel);
         entradaPainel.add(dataVencimentoField);
-        entradaPainel.add(adicionarTarefaButton);
+        entradaPainel.add(new JLabel("Estado:"));
+        entradaPainel.add(estadoComboBox);
 
         JPanel acoesPainel = new JPanel();
         JButton excluirTarefaButton = new JButton("Excluir Tarefa");
@@ -91,11 +93,12 @@ public class GerenciadorTarefasComLoginGUI {
                 atualizarTarefa();
             }
         });
-        acoesPainel.add(excluirTarefaButton);
-        acoesPainel.add(atualizarTarefaButton);
 
         painel.add(entradaPainel, BorderLayout.NORTH);
         painel.add(new JScrollPane(tarefaTable), BorderLayout.CENTER);
+        acoesPainel.add(excluirTarefaButton);
+        acoesPainel.add(atualizarTarefaButton);
+        acoesPainel.add(adicionarTarefaButton);  // Adicione o botão de adicionar tarefa
         painel.add(acoesPainel, BorderLayout.SOUTH);
 
         frame.add(painel);
@@ -104,82 +107,15 @@ public class GerenciadorTarefasComLoginGUI {
     }
 
     private void criarTelaDeLogin() {
-        loginPanel = new JPanel(new GridBagLayout());
-        loginPanel.setBackground(new Color(0, 102, 204));
-
-        JLabel titleLabel = new JLabel("Bem-vindo ao Gerenciador de Tarefas");
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 20));
-
-        usernameField = new JTextField(15);
-        passwordField = new JPasswordField(15);
-
-        JButton loginButton = new JButton("Login");
-        loginButton.setBackground(new Color(0, 153, 51));
-        loginButton.setForeground(Color.WHITE);
-
-        loginButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                fazerLogin();
-            }
-        });
-
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridwidth = 2;
-        gbc.insets = new Insets(20, 0, 20, 0);
-
-        loginPanel.add(titleLabel, gbc);
-
-        gbc.gridy++;
-        gbc.gridwidth = 1;
-        gbc.insets = new Insets(0, 0, 10, 10);
-        loginPanel.add(new JLabel("Nome de Usuário:"), gbc);
-
-        gbc.gridx++;
-        loginPanel.add(usernameField, gbc);
-
-        gbc.gridx = 0;
-        gbc.gridy++;
-        loginPanel.add(new JLabel("Senha:"), gbc);
-
-        gbc.gridx++;
-        loginPanel.add(passwordField, gbc);
-
-        gbc.gridx = 0;
-        gbc.gridy++;
-        gbc.gridwidth = 2;
-        loginPanel.add(loginButton, gbc);
-
-        frame.add(loginPanel);
-        frame.revalidate();
-        frame.repaint();
-    }
-
-    private void fazerLogin() {
-        String username = usernameField.getText();
-        char[] password = passwordField.getPassword();
-        String storedUsername = "user"; // Nome de usuário de exemplo
-        char[] storedPassword = "password".toCharArray(); // Senha de exemplo
-
-        if (username.equals(storedUsername) && java.util.Arrays.equals(password, storedPassword)) {
-            loggedIn = true;
-            usernameField.setText("");
-            passwordField.setText("");
-            frame.getContentPane().remove(loginPanel);
-            frame.revalidate();
-            frame.repaint();
-            criarInterfacePrincipal();
-        } else {
-            JOptionPane.showMessageDialog(frame, "Login falhou. Tente novamente.", "Erro de Login", JOptionPane.ERROR_MESSAGE);
-        }
+        // Simulação de login, você pode implementar seu próprio método de autenticação aqui.
+        loggedIn = true;
     }
 
     private void adicionarTarefa() {
         String nome = nomeTarefaField.getText();
         String descricao = descricaoTarefaField.getText();
         String dataVencimentoStr = dataVencimentoField.getText();
+        String estado = (String) estadoComboBox.getSelectedItem();
 
         if (!nome.isEmpty()) {
             SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
@@ -191,9 +127,9 @@ public class GerenciadorTarefasComLoginGUI {
                 // Tratamento de erro caso a data seja inválida
             }
 
-            Tarefa tarefa = new Tarefa(nome, descricao, dataVencimento);
+            Tarefa tarefa = new Tarefa(nome, descricao, dataVencimento, estado);
             tarefas.add(tarefa);
-            tarefaTableModel.addRow(new Object[]{tarefa.getNome(), tarefa.getDescricao(), dateFormat.format(dataVencimento)});
+            tarefaTableModel.addRow(new Object[]{tarefa.getNome(), tarefa.getDescricao(), dateFormat.format(dataVencimento), tarefa.getEstado()});
             nomeTarefaField.setText("");
             descricaoTarefaField.setText("");
             dataVencimentoField.setText("");
@@ -214,6 +150,7 @@ public class GerenciadorTarefasComLoginGUI {
             String nome = nomeTarefaField.getText();
             String descricao = descricaoTarefaField.getText();
             String dataVencimentoStr = dataVencimentoField.getText();
+            String estado = (String) estadoComboBox.getSelectedItem();
 
             if (!nome.isEmpty()) {
                 SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
@@ -229,10 +166,12 @@ public class GerenciadorTarefasComLoginGUI {
                 tarefa.setNome(nome);
                 tarefa.setDescricao(descricao);
                 tarefa.setDataVencimento(dataVencimento);
+                tarefa.setEstado(estado);
 
                 tarefaTableModel.setValueAt(tarefa.getNome(), selectedRow, 0);
                 tarefaTableModel.setValueAt(tarefa.getDescricao(), selectedRow, 1);
                 tarefaTableModel.setValueAt(dateFormat.format(dataVencimento), selectedRow, 2);
+                tarefaTableModel.setValueAt(estado, selectedRow, 3);
             }
         }
     }
@@ -243,15 +182,16 @@ public class GerenciadorTarefasComLoginGUI {
 }
 
 class Tarefa {
-
     private String nome;
     private String descricao;
     private Date dataVencimento;
+    private String estado;
 
-    public Tarefa(String nome, String descricao, Date dataVencimento) {
+    public Tarefa(String nome, String descricao, Date dataVencimento, String estado) {
         this.nome = nome;
         this.descricao = descricao;
         this.dataVencimento = dataVencimento;
+        this.estado = estado;
     }
 
     public String getNome() {
@@ -276,5 +216,13 @@ class Tarefa {
 
     public void setDataVencimento(Date dataVencimento) {
         this.dataVencimento = dataVencimento;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
     }
 }
