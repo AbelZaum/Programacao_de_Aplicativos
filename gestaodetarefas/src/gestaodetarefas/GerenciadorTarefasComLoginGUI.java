0d27c56dca325c47e0ec0089f6c14ff9f7ef3cfb
Gabriel Abel da Silva <127804266+AbelZaum@users.noreply.github.com>
2023-11-09 10:11:08 -0300

    Update GerenciadorTarefasComLoginGUI.java

--- gestaodetarefas/src/gestaodetarefas/GerenciadorTarefasComLoginGUI.java
+++ gestaodetarefas/src/gestaodetarefas/GerenciadorTarefasComLoginGUI.java
@@ -1,59 +1,51 @@
+package gestaodetarefas;
+
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class GerenciadorTarefasComLoginGUI {
+public class GerenciadorTarefasTrelloGUI {
 
     private JFrame frame;
+    private JComboBox<String> colunaComboBox;
+    private JButton moverTarefaButton;
+    private JButton adicionarTarefaButton;
+    private JButton excluirTarefaButton;  // Novo botão para excluir tarefa
     private JTextField nomeTarefaField;
     private JTextField descricaoTarefaField;
     private JTextField dataVencimentoField;
-    private DefaultTableModel tarefaTableModel;
-    private JTable tarefaTable;
-    private List<Tarefa> tarefas;
+    private Map<String, DefaultTableModel> colunaTabelas;
+    private Map<String, List<Tarefa>> colunasTarefas;
 
-    private JComboBox<String> estadoComboBox;  // JComboBox para selecionar o estado
-    private JButton adicionarTarefaButton;  // Botão para adicionar tarefas
-    private boolean loggedIn = true;  // Defina como true para evitar a tela de login
-
-    public GerenciadorTarefasComLoginGUI() {
+    public GerenciadorTarefasTrelloGUI() {
         frame = new JFrame("Gerenciador de Tarefas");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 600);
+        frame.setSize(1200, 600);
 
-        tarefas = new ArrayList<>();
-        tarefaTableModel = new DefaultTableModel(new String[]{"Nome", "Descrição", "Data de Vencimento", "Estado"}, 0);
-        tarefaTable = new JTable(tarefaTableModel);
-        tarefaTable.setRowHeight(40);
-        tarefaTable.getTableHeader().setReorderingAllowed(false);
+        colunaTabelas = new HashMap<>();
+        colunasTarefas = new HashMap<>();
 
-        if (!loggedIn) {
-            criarTelaDeLogin();
-        } else {
-            criarInterfacePrincipal();
+        for (String coluna : new String[]{"A fazer", "Em andamento", "Concluídas"}) {
+            colunaTabelas.put(coluna, new DefaultTableModel(new String[]{"Nome", "Descrição", "Data de Vencimento"}, 0));
+            colunasTarefas.put(coluna, new java.util.ArrayList<>());
         }
 
-        frame.setVisible(true);
-    }
+        colunaComboBox = new JComboBox<>(new String[]{"A fazer", "Em andamento", "Concluídas"});
 
-    private void criarInterfacePrincipal() {
-        JPanel painel = new JPanel(new BorderLayout());
-
-        JPanel entradaPainel = new JPanel(new GridLayout(4, 2));
-        JLabel nomeTarefaLabel = new JLabel("Nome da Tarefa:");
-        nomeTarefaField = new JTextField();
-        JLabel descricaoTarefaLabel = new JLabel("Descrição da Tarefa:");
-        descricaoTarefaField = new JTextField();
-        JLabel dataVencimentoLabel = new JLabel("Data de Vencimento (dd/MM/yyyy):");
-        dataVencimentoField = new JTextField();
-
-        estadoComboBox = new JComboBox<>(new String[]{"A fazer", "Em andamento", "Concluídas"});
+        moverTarefaButton = new JButton("Mover Tarefa");
+        moverTarefaButton.setBackground(Color.BLUE);
+        moverTarefaButton.setForeground(Color.WHITE);
+        moverTarefaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                moverTarefa();
+            }
+        });
 
         adicionarTarefaButton = new JButton("Adicionar Tarefa");
         adicionarTarefaButton.setBackground(Color.GREEN);
@@ -61,21 +53,11 @@ public class GerenciadorTarefasComLoginGUI {
         adicionarTarefaButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                adicionarTarefa();
+                adicionarTarefaAFAzer();
             }
         });
 
-        entradaPainel.add(nomeTarefaLabel);
-        entradaPainel.add(nomeTarefaField);
-        entradaPainel.add(descricaoTarefaLabel);
-        entradaPainel.add(descricaoTarefaField);
-        entradaPainel.add(dataVencimentoLabel);
-        entradaPainel.add(dataVencimentoField);
-        entradaPainel.add(new JLabel("Estado:"));
-        entradaPainel.add(estadoComboBox);
-
-        JPanel acoesPainel = new JPanel();
-        JButton excluirTarefaButton = new JButton("Excluir Tarefa");
+        excluirTarefaButton = new JButton("Excluir Tarefa");  // Inicializa o botão de excluir tarefa
         excluirTarefaButton.setBackground(Color.RED);
         excluirTarefaButton.setForeground(Color.WHITE);
         excluirTarefaButton.addActionListener(new ActionListener() {
@@ -84,145 +66,126 @@ public class GerenciadorTarefasComLoginGUI {
                 excluirTarefa();
             }
         });
-        JButton atualizarTarefaButton = new JButton("Atualizar Tarefa");
-        atualizarTarefaButton.setBackground(Color.BLUE);
-        atualizarTarefaButton.setForeground(Color.WHITE);
-        atualizarTarefaButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                atualizarTarefa();
-            }
-        });
 
-        painel.add(entradaPainel, BorderLayout.NORTH);
-        painel.add(new JScrollPane(tarefaTable), BorderLayout.CENTER);
-        acoesPainel.add(excluirTarefaButton);
-        acoesPainel.add(atualizarTarefaButton);
-        acoesPainel.add(adicionarTarefaButton);  // Adicione o botão de adicionar tarefa
-        painel.add(acoesPainel, BorderLayout.SOUTH);
+        nomeTarefaField = new JTextField();
+        descricaoTarefaField = new JTextField();
+        dataVencimentoField = new JTextField();
 
-        frame.add(painel);
-        frame.revalidate();
-        frame.repaint();
+        criarInterfacePrincipal();
+
+        frame.setVisible(true);
     }
 
-    private void criarTelaDeLogin() {
-        // Simulação de login, você pode implementar seu próprio método de autenticação aqui.
-        loggedIn = true;
+    private void criarInterfacePrincipal() {
+        JPanel painelPrincipal = new JPanel(new GridLayout(1, 3));
+
+        for (String coluna : colunaTabelas.keySet()) {
+            JPanel colunaPanel = new JPanel(new BorderLayout());
+            colunaPanel.setBorder(BorderFactory.createTitledBorder(coluna));
+
+            JTable tarefaTable = new JTable(colunaTabelas.get(coluna));
+            colunaPanel.add(new JScrollPane(tarefaTable), BorderLayout.CENTER);
+
+            painelPrincipal.add(colunaPanel);
+        }
+
+        JPanel adicionarPanel = new JPanel(new GridLayout(7, 1));
+        adicionarPanel.setBorder(BorderFactory.createTitledBorder("Adicionar Tarefa"));
+
+        adicionarPanel.add(new JLabel("Nome:"));
+        adicionarPanel.add(nomeTarefaField);
+        adicionarPanel.add(new JLabel("Descrição:"));
+        adicionarPanel.add(descricaoTarefaField);
+        adicionarPanel.add(new JLabel("Data de Vencimento (dd/MM/yyyy):"));
+        adicionarPanel.add(dataVencimentoField);
+        adicionarPanel.add(adicionarTarefaButton);
+
+        JPanel controlePanel = new JPanel(new FlowLayout());
+        controlePanel.add(colunaComboBox);
+        controlePanel.add(moverTarefaButton);
+        controlePanel.add(excluirTarefaButton);  // Adiciona o botão de excluir tarefa ao painel de controle
+
+        frame.add(painelPrincipal, BorderLayout.CENTER);
+        frame.add(adicionarPanel, BorderLayout.WEST);
+        frame.add(controlePanel, BorderLayout.SOUTH);
     }
 
-    private void adicionarTarefa() {
+    private void moverTarefa() {
+        String colunaAtual = (String) colunaComboBox.getSelectedItem();
+        String proximaColuna = obterProximaColuna(colunaAtual);
+
+        int selectedRow = colunaTabelas.get(colunaAtual).getRowCount() - 1;
+        if (selectedRow >= 0 && proximaColuna != null) {
+            Tarefa tarefa = colunasTarefas.get(colunaAtual).remove(selectedRow);
+            colunasTarefas.get(proximaColuna).add(tarefa);
+
+            colunaTabelas.get(colunaAtual).removeRow(selectedRow);
+            colunaTabelas.get(proximaColuna).addRow(new Object[]{tarefa.getNome(), tarefa.getDescricao(), tarefa.getDataVencimentoStr()});
+        }
+    }
+
+    private void excluirTarefa() {
+        String colunaAtual = (String) colunaComboBox.getSelectedItem();
+        int selectedRow = colunaTabelas.get(colunaAtual).getRowCount() - 1;
+        if (selectedRow >= 0) {
+            colunasTarefas.get(colunaAtual).remove(selectedRow);
+            colunaTabelas.get(colunaAtual).removeRow(selectedRow);
+        }
+    }
+
+    private String obterProximaColuna(String colunaAtual) {
+        switch (colunaAtual) {
+            case "A fazer":
+                return "Em andamento";
+            case "Em andamento":
+                return "Concluídas";
+            default:
+                return null;
+        }
+    }
+
+    private void adicionarTarefaAFAzer() {
         String nome = nomeTarefaField.getText();
         String descricao = descricaoTarefaField.getText();
-        String dataVencimentoStr = dataVencimentoField.getText();
-        String estado = (String) estadoComboBox.getSelectedItem();
+        String dataVencimento = dataVencimentoField.getText();
 
         if (!nome.isEmpty()) {
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-            Date dataVencimento = null;
-
-            try {
-                dataVencimento = dateFormat.parse(dataVencimentoStr);
-            } catch (Exception ex) {
-                // Tratamento de erro caso a data seja inválida
-            }
-
-            Tarefa tarefa = new Tarefa(nome, descricao, dataVencimento, estado);
-            tarefas.add(tarefa);
-            tarefaTableModel.addRow(new Object[]{tarefa.getNome(), tarefa.getDescricao(), dateFormat.format(dataVencimento), tarefa.getEstado()});
+            Tarefa tarefa = new Tarefa(nome, descricao, dataVencimento);
+            colunasTarefas.get("A fazer").add(tarefa);
+            DefaultTableModel tabelaAtual = colunaTabelas.get("A fazer");
+            tabelaAtual.addRow(new Object[]{tarefa.getNome(), tarefa.getDescricao(), tarefa.getDataVencimentoStr()});
             nomeTarefaField.setText("");
             descricaoTarefaField.setText("");
             dataVencimentoField.setText("");
         }
     }
 
-    private void excluirTarefa() {
-        int selectedRow = tarefaTable.getSelectedRow();
-        if (selectedRow != -1) {
-            tarefas.remove(selectedRow);
-            tarefaTableModel.removeRow(selectedRow);
-        }
-    }
-
-    private void atualizarTarefa() {
-        int selectedRow = tarefaTable.getSelectedRow();
-        if (selectedRow != -1) {
-            String nome = nomeTarefaField.getText();
-            String descricao = descricaoTarefaField.getText();
-            String dataVencimentoStr = dataVencimentoField.getText();
-            String estado = (String) estadoComboBox.getSelectedItem();
-
-            if (!nome.isEmpty()) {
-                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-                Date dataVencimento = null;
-
-                try {
-                    dataVencimento = dateFormat.parse(dataVencimentoStr);
-                } catch (Exception ex) {
-                    // Tratamento de erro caso a data seja inválida
-                }
-
-                Tarefa tarefa = tarefas.get(selectedRow);
-                tarefa.setNome(nome);
-                tarefa.setDescricao(descricao);
-                tarefa.setDataVencimento(dataVencimento);
-                tarefa.setEstado(estado);
-
-                tarefaTableModel.setValueAt(tarefa.getNome(), selectedRow, 0);
-                tarefaTableModel.setValueAt(tarefa.getDescricao(), selectedRow, 1);
-                tarefaTableModel.setValueAt(dateFormat.format(dataVencimento), selectedRow, 2);
-                tarefaTableModel.setValueAt(estado, selectedRow, 3);
-            }
-        }
-    }
-
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new GerenciadorTarefasComLoginGUI());
-    }
-}
-
-class Tarefa {
-    private String nome;
-    private String descricao;
-    private Date dataVencimento;
-    private String estado;
-
-    public Tarefa(String nome, String descricao, Date dataVencimento, String estado) {
-        this.nome = nome;
-        this.descricao = descricao;
-        this.dataVencimento = dataVencimento;
-        this.estado = estado;
+        SwingUtilities.invokeLater(() -> new GerenciadorTarefasTrelloGUI());
     }
 
-    public String getNome() {
-        return nome;
-    }
+    class Tarefa {
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+        private String nome;
+        private String descricao;
+        private String dataVencimento;
 
-    public String getDescricao() {
-        return descricao;
-    }
+        public Tarefa(String nome, String descricao, String dataVencimento) {
+            this.nome = nome;
+            this.descricao = descricao;
+            this.dataVencimento = dataVencimento;
+        }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+        public String getNome() {
+            return nome;
+        }
 
-    public Date getDataVencimento() {
-        return dataVencimento;
-    }
+        public String getDescricao() {
+            return descricao;
+        }
 
-    public void setDataVencimento(Date dataVencimento) {
-        this.dataVencimento = dataVencimento;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
+        public String getDataVencimentoStr() {
+            return dataVencimento;
+        }
     }
 }
